a3f93ffc123e9a94986ded5d51db9874281fb531
Albert Omware <aomware@gmail.com>
2019-07-11 11:57:03 +0300

    added new branch

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,13 +6,23 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item href="/#/get-started/">Get started</b-nav-item>
-                    <b-nav-item href="/#/">My Chatbot</b-nav-item>
-                    <b-nav-item href="/#/inbox/">Inbox</b-nav-item>
-                    <b-nav-item href="/#/contacts/">Contacts</b-nav-item>
+                <b-nav-item v-b-popover.hover="'Documentation on how to get started'"href="/#/get-started/">Get started</b-nav-item>
+                <b-navbar-nav class="ml-auto" v-b-popover.hover="'Chatbot configurations'">
+                    <b-nav-item-dropdown left>
+                    <template slot="button-content"><em>Chat Bot</em></template>
+                    <b-dropdown-item href="/#/bot_modules/">Bot Modules</b-dropdown-item>
+                    <b-dropdown-item href="/#/module_details/">Module Details</b-dropdown-item>
+                    <b-dropdown-item href="/#/new_bot/">New Bot</b-dropdown-item>
+                    </b-nav-item-dropdown>
                 </b-navbar-nav>
-
-                <b-navbar-nav class="ml-auto">
+                <b-nav-item v-b-popover.hover="'Check Inbox messages'"href="/#/inbox/">Inbox</b-nav-item>
+                <b-nav-item-dropdown v-b-popover.hover="'Contacts list and creations'">
+                    <template slot="button-content"><em>Contacts </em></template>
+                    <b-dropdown-item href="/#/create_contacts/">Create Contacts</b-dropdown-item>
+                    <b-dropdown-item href="/#/get_contacts/">Get Contacts</b-dropdown-item>
+                    <b-dropdown-item href="/#/update_contacts/">Update Contacts</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav><b-navbar-nav class="ml-auto" >
                     <b-nav-item-dropdown right>
                     <template slot="button-content"><em>Account</em></template>
                     <b-dropdown-item href="/#/profile/">Profile</b-dropdown-item>
@@ -27,6 +37,7 @@
 </template>
 
 <script>
+
 export default {
 
 }
