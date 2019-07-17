717bedc82f1b1c0d9ae820c461b52d50ac27535b
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2019-07-17 14:51:13 +0300

    Partial responses modals

--- src/components/bots/NewResponse.vue
+++ src/components/bots/NewResponse.vue
@@ -1,16 +1,15 @@
 <template>
-    <b-modal v-model="show" hide-footer title="New Response" @hidden="hideModal">
+    <b-modal v-model="show_modal" hide-footer title="New Response" @hidden="hideModal">
         <form @submit="submitForm" action="javascript:;">
             <div class="form-group">
               <label class="control-label">Text Content</label>
               <input
                 name="text"
                 class="form-control"
-                v-validate="'required|text'"
+                v-validate="'required'"
                 type="text"
                 v-model="form.content.text"
-                placeholder="Enter text content"
-              />
+                placeholder="Enter text"/>
               <div class="help-block text-danger">
                 <span>{{ errors.first('text') }}</span>
               </div>
@@ -19,18 +18,14 @@
             <div class="form-group">
               <label class="control-label">Quick Replies Content</label>
               <input
-                name="quick replies"
-                class="form-control"
-                v-validate="'required|text'"
-                type="text"
-                v-model="form.content.quick_replies"
+                name="quick replies" 
+                class="form-control" 
+                type="text" 
+                v-model="form.content.quick_replies" 
                 placeholder="Enter a quick reply"
               />
-              <div class="help-block text-danger">
-                <span>{{ errors.first('quick_replies') }}</span>
-              </div>
             </div>
-            
+
             <div class="action">
               <button type="submit" class="btn btn-primary">Create Response</button>
             </div>
@@ -39,10 +34,12 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
-            show: true,
+            show_modal: true,
             form: {
                 content: {
                 text: "",
@@ -63,24 +60,28 @@ export default {
         },
         submitForm() {
             let self = this;
-            let payload = this.form;
-            let loader = self.$loading.show();
-            payload["module_id"] = this.module_id;
-            axios.post("responses/", payload)
-                .then(resp => {
-                    self.$router.push({ name: "responses_list" });
-                    loader.hide();
-                })
-                .catch(err => {
-                    loader.hide();
-                    console.log("Error", err);
-                    self.$notify({
-                        group: "default",
-                        type: "error",
-                        title: err,
-                        text: err.response.data
-                    });
-                });
+            this.$validator.validate().then(valid => {
+                if (valid) {
+                    let payload = this.form;
+                    let loader = self.$loading.show();
+                    payload["module_id"] = this.module_id;
+                    axios.post("responses/", payload)
+                        .then(resp => {
+                            loader.hide();
+                            self.show_modal = false;
+                        })
+                        .catch(err => {
+                            loader.hide();
+                            console.log("Error", err);
+                            self.$notify({
+                                group: "default",
+                                type: "error",
+                                title: err,
+                                text: err.response.data
+                            });
+                        });
+                }
+            })
         }
     }
 }
