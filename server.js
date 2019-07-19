8541c2b745d38c8ca19e82f5cf5704c92879b1f3
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2019-07-19 18:51:27 +0300

    Heroku deployment

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = process.env.PORT || 5000
+
+app.use(express.static('dist'))
+
+app.listen(port, () => console.log('listening on port:', port))
